f99ccccf993995174a250fdc0bbdc0958bfe8dd7
yoga <11211086@student.itk.ac.id>
2023-09-07 12:13:02 +0800

    Create server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,42 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 3000;
+const HOST = "localhost";
+const HOST_URL = `http://${HOST}:${PORT}`
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, {
+    'Content-Type': [
+      "text/css",
+      "image/png",
+      "image/svg+xml",
+      "text/html",
+      "text/javascript",
+    ]
+  });
+
+  const url = req.url;
+
+  switch (url) {
+    case '/find-car':
+      res.write("halaman cari mobil");
+      res.end();
+      break;
+    default:
+      fs.readFile('public/index.example.html', (error, data) => {
+        if (error) {
+          res.writeHead(404);
+          res.write('error: file not found');
+        } else {
+          res.write(data);
+        }
+        res.end();
+      })
+      break;
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`Localhost is running : ${HOST_URL}`)
+})
